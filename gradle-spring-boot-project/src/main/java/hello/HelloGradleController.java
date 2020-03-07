98fd4e7c818d0984cf33f5abf43f0d15d08898a1
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2020-03-07 11:38:03 -0700

    gradle build example

---
+++ gradle-spring-boot-project/src/main/java/hello/HelloGradleController.java
@@ -0,0 +1,13 @@
+package hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class HelloGradleController {
+
+    @GetMapping
+    public String helloGradle() {
+        return "Hello Gradle!";
+    }
+}
